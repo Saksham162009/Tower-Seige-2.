c4ef56fe246a46cc935e242add4aa1f7f1cf2418
Saksham162009 <85797538+Saksham162009@users.noreply.github.com>
2021-09-08 14:24:06 +0530

    Add files via upload

---
+++ Block2.js
@@ -0,0 +1,41 @@
+class Block2{
+    constructor(x, y, width, height) {
+        var options = {
+            'restitution':0.4,
+            'friction':0.1
+           
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.visibility=255
+        World.add(world, this.body);
+
+      }
+      display(){
+        push();
+        //translate(this.body.position.x, this.body.position.y);
+        var pos =this.body.position;
+        rectMode(CENTER);
+        fill(rgb(240,128,128));
+        rect(pos.x, pos.y, this.width, this.height);
+        if(this.body.speed <2){
+          var angle = this.body.angle;
+          var pos= this.body.position;
+          push();
+          translate(pos.x, pos.y);
+          rotate(angle);
+          rectMode(CENTER);
+          rect(0,0,this.width, this.height);
+          pop();
+        }else{
+          World.remove(world, this.body);
+          push();
+          this.visibility = this.visibility -5;
+          pop();
+          
+        }
+        
+        pop();
+      }
+  }
